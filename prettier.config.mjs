f6b1fe8d72ead1c2025f29ae3197f53c1e2a3802
Denilson dos Santos Ebling <d.ebling8@gmail.com>
2024-01-30 13:31:25 -0300

    Initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Config} */
+export default {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
